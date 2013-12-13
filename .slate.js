46756b407f497e9e3aa5e963f9d3563f2269b70c
Clemens Tolboom <clemens@build2be.com>
2013-12-13 13:26:39 +0100

    Add all numpad # keys.

--- .slate.js
+++ .slate.js
@@ -1,44 +1,13 @@
 // Create Operations
-var pushRight = slate.operation("push", {
-  "direction" : "right",
-  "style" : "bar-resize:screenSizeX/3"
-});
 
-var pushRight2 = slate.operation("push", {
-  "direction" : "right",
-  "style" : "bar-resize:screenSizeX/3*2"
-});
+// Resizes are bound to numpad CTRL-# and CTRL-SHIFT-#
+//
+// 7 8 9
+// 4 5 6
+// 1 2 3
+// 0
 
-var pushLeft = slate.operation("push", {
-  "direction" : "left",
-  "style" : "bar-resize:screenSizeX/3"
-});
-
-var pushLeft2 = slate.operation("push", {
-  "direction" : "left",
-  "style" : "bar-resize:screenSizeX/3*2"
-});
-
-var pushTop = slate.operation("push", {
-  "direction" : "top",
-  "style" : "bar-resize:screenSizeY/3"
-});
-
-var pushTop2 = slate.operation("push", {
-  "direction" : "top",
-  "style" : "bar-resize:screenSizeY/3*2"
-});
-
-var pushBottom = slate.operation("push", {
-  "direction" : "bottom",
-  "style" : "bar-resize:screenSizeY/3"
-});
-
-var pushBottom2 = slate.operation("push", {
-  "direction" : "bottom",
-  "style" : "bar-resize:screenSizeY/3*2"
-});
-
+// 0
 var fullscreen = slate.operation("move", {
   "x" : "screenOriginX",
   "y" : "screenOriginY",
@@ -46,13 +15,61 @@ var fullscreen = slate.operation("move", {
   "height" : "screenSizeY"
 });
 
+// 1
+var pushBottomLeft = slate.operation("move", {
+  "x" : "screenOriginX",
+  "y" : "screenSizeY/3*2",
+  "width" : "screenSizeX/3",
+  "height" : "screenSizeY/3"
+});
+var pushBottomLeft2 = slate.operation("move", {
+  "x" : "screenOriginX",
+  "y" : "screenSizeY/3",
+  "width" : "screenSizeX/3*2",
+  "height" : "screenSizeY/3*2"
+});
+
+// 2
+var pushBottom = slate.operation("push", {
+  "direction" : "bottom",
+  "style" : "bar-resize:screenSizeY/3"
+});
+var pushBottom2 = slate.operation("push", {
+  "direction" : "bottom",
+  "style" : "bar-resize:screenSizeY/3*2"
+});
+
+// 3
+var pushBottomRight = slate.operation("move", {
+  "x" : "screenSizeX/3*2",
+  "y" : "screenSizeY/3*2",
+  "width" : "screenSizeX/3",
+  "height" : "screenSizeY/3"
+});
+var pushBottomRight2 = slate.operation("move", {
+  "x" : "screenSizeX/3",
+  "y" : "screenSizeY/3",
+  "width" : "screenSizeX/3*2",
+  "height" : "screenSizeY/3*2"
+});
+
+// 4
+var pushLeft = slate.operation("push", {
+  "direction" : "left",
+  "style" : "bar-resize:screenSizeX/3"
+});
+var pushLeft2 = slate.operation("push", {
+  "direction" : "left",
+  "style" : "bar-resize:screenSizeX/3*2"
+});
+
+// 5
 var pushCenter = slate.operation("move", {
   "x" : "screenSizeX/3",
   "y" : "screenSizeY/3",
   "width" : "screenSizeX/3",
   "height" : "screenSizeY/3"
 });
-
 var pushCenter2 = slate.operation("move", {
   "x" : "screenSizeX/6",
   "y" : "screenSizeY/6",
@@ -60,26 +77,84 @@ var pushCenter2 = slate.operation("move", {
   "height" : "screenSizeY/3*2"
 });
 
-// Bind A Crazy Function to 1+ctrl
+// 6
+var pushRight = slate.operation("push", {
+  "direction" : "right",
+  "style" : "bar-resize:screenSizeX/3"
+});
+var pushRight2 = slate.operation("push", {
+  "direction" : "right",
+  "style" : "bar-resize:screenSizeX/3*2"
+});
+
+// 7
+var pushTopLeft = slate.operation("move", {
+  "x" : "screenOriginX",
+  "y" : "screenOriginY",
+  "width" : "screenSizeX/3",
+  "height" : "screenSizeY/3"
+});
+var pushTopLeft2 = slate.operation("move", {
+  "x" : "screenOriginX",
+  "y" : "screenOriginY",
+  "width" : "screenSizeX * 2 / 3",
+  "height" : "screenSizeY * 2 / 3"
+});
+
+
+// 8
+var pushTop = slate.operation("push", {
+  "direction" : "top",
+  "style" : "bar-resize:screenSizeY/3"
+});
+var pushTop2 = slate.operation("push", {
+  "direction" : "top",
+  "style" : "bar-resize:screenSizeY/3*2"
+});
+
+// 9
+var pushTopRight = slate.operation("move", {
+  "x" : "screenSizeX/3*2",
+  "y" : "screenOriginX",
+  "width" : "screenSizeX/3",
+  "height" : "screenSizeY/3"
+});
+var pushTopRight2 = slate.operation("move", {
+  "x" : "screenSizeX/3",
+  "y" : "screenOriginX",
+  "width" : "screenSizeX/3*2",
+  "height" : "screenSizeY/3*2"
+});
+
 slate.bind("pad0:ctrl", function(win) {
   win.doOperation(fullscreen);
 });
 
-slate.bind("pad5:ctrl", function(win) {
-  win.doOperation(pushCenter);
+// 1
+slate.bind("pad1:ctrl", function(win) {
+    win.doOperation(pushBottomLeft);
+});
+slate.bind("pad1:shift,ctrl", function(win) {
+    win.doOperation(pushBottomLeft2);
 });
 
-slate.bind("pad5:shift,ctrl", function(win) {
-  win.doOperation(pushCenter2);
+// 2
+slate.bind("pad2:ctrl", function(win) {
+    win.doOperation(pushBottom);
+});
+slate.bind("pad2:shift,ctrl", function(win) {
+    win.doOperation(pushBottom2);
 });
 
-slate.bind("pad6:ctrl", function(win) {
-    win.doOperation(pushRight);
+// 3
+slate.bind("pad3:ctrl", function(win) {
+    win.doOperation(pushBottomRight);
 });
-slate.bind("pad6:shift,ctrl", function(win) {
-    win.doOperation(pushRight2);
+slate.bind("pad3:shift,ctrl", function(win) {
+    win.doOperation(pushBottomRight2);
 });
 
+// 4
 slate.bind("pad4:ctrl", function(win) {
     win.doOperation(pushLeft);
 });
@@ -87,6 +162,31 @@ slate.bind("pad4:shift,ctrl", function(win) {
     win.doOperation(pushLeft2);
 });
 
+// 5
+slate.bind("pad5:ctrl", function(win) {
+  win.doOperation(pushCenter);
+});
+slate.bind("pad5:shift,ctrl", function(win) {
+  win.doOperation(pushCenter2);
+});
+
+// 6
+slate.bind("pad6:ctrl", function(win) {
+    win.doOperation(pushRight);
+});
+slate.bind("pad6:shift,ctrl", function(win) {
+    win.doOperation(pushRight2);
+});
+
+// 7
+slate.bind("pad7:ctrl", function(win) {
+    win.doOperation(pushTopLeft);
+});
+slate.bind("pad7:shift,ctrl", function(win) {
+    win.doOperation(pushTopLeft2);
+});
+
+// 8
 slate.bind("pad8:ctrl", function(win) {
     win.doOperation(pushTop);
 });
@@ -94,13 +194,14 @@ slate.bind("pad8:shift,ctrl", function(win) {
     win.doOperation(pushTop2);
 });
 
-slate.bind("pad2:ctrl", function(win) {
-    win.doOperation(pushBottom);
+// 9
+slate.bind("pad9:ctrl", function(win) {
+    win.doOperation(pushTopRight);
+});
+slate.bind("pad9:shift,ctrl", function(win) {
+    win.doOperation(pushTopRight2);
 });
 
-slate.bind("pad2:shift,ctrl", function(win) {
-    win.doOperation(pushBottom2);
-});
 
 slate.configAll({
   "defaultToCurrentScreen": true,
